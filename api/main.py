baac157dca89ac3fc837a9600d4d059a2a7618df
Matt R <m.remigio331@gmail.com>
2026-04-26 15:43:18 -0700

    Adding api stack.

---
+++ api/main.py
@@ -0,0 +1,24 @@
+from helpers.logger import setup_logging
+
+setup_logging()
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from middleware.service_log import ServiceLogMiddleware
+from endpoints.home import router as home_router
+from endpoints.system import router as system_router
+from endpoints.stonks import router as stonks_router
+from endpoints.mta import router as mta_router
+
+app = FastAPI(title="Nuggies Pi Displays", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+app.add_middleware(ServiceLogMiddleware)
+
+app.include_router(home_router)
+app.include_router(system_router)
+app.include_router(stonks_router)
+app.include_router(mta_router)
